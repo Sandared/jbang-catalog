61b7a34c1932b4763b7b1488e9723b1883d1c91e
Thomas Driessen <thomas.driessen.td@gmail.com>
2023-02-06 20:35:17 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A collection of qbilon jbang commands
---
+++ src/main/java/io/qbilon/jbang/Test.java
@@ -0,0 +1,9 @@
+package io.qbilon.jbang;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
